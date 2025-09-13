c23e927ba3f07b904678a226e209c7788b3043f0
Giovane Aires Gomes <giovaneag17@outlook.com>
2025-09-13 18:11:41 -0300

    feat: update AuthRouter and LoginScreen with improved layout and styles

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,11 +1,7 @@
-import { HomeScreen } from "../screens/home";
-import { ExerciseScreen } from "../screens/exercises";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { LoginScreen } from "../screens/login";
 import { SignUpScreen } from "../screens/sign-up";
 import { ResetPasswordScreen } from "../screens/reset-password";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
-import { View } from "react-native";
 
 export type AuthRoutes = {
   login: undefined;
@@ -16,29 +12,16 @@ export type AuthRoutes = {
 const Stack = createNativeStackNavigator<AuthRoutes>();
 
 export function AuthRouter() {
-  const insets = useSafeAreaInsets();
-
   return (
-    <View
-      style={{
-        flex: 1,
-        paddingTop: insets.top,
-        paddingBottom: insets.bottom,
-        paddingLeft: insets.left,
-        paddingRight: insets.right,
+    <Stack.Navigator
+      initialRouteName="login"
+      screenOptions={{
+        headerShown: false,
       }}
     >
-      <Stack.Navigator
-        initialRouteName="login"
-        screenOptions={{
-          title: "Health App",
-          headerTitleAlign: "center",
-        }}
-      >
-        <Stack.Screen name="login" component={LoginScreen} />
-        <Stack.Screen name="signUp" component={SignUpScreen} />
-        <Stack.Screen name="resetPassword" component={ResetPasswordScreen} />
-      </Stack.Navigator>
-    </View>
+      <Stack.Screen name="login" component={LoginScreen} />
+      <Stack.Screen name="signUp" component={SignUpScreen} />
+      <Stack.Screen name="resetPassword" component={ResetPasswordScreen} />
+    </Stack.Navigator>
   );
 }
--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,9 +1,32 @@
-import { Text, View } from "react-native";
+import { Button, TextInput, View } from "react-native";
+import styles from "./styles";
 
 export function LoginScreen() {
   return (
-    <View style={{ flex: 1 }}>
-      <Text>Login</Text>
+    <View style={styles.container}>
+      <TextInput
+        placeholder="Usuário"
+        style={{
+          height: 54,
+          borderColor: "#000",
+          borderWidth: 1,
+          borderRadius: 7,
+          paddingHorizontal: 20,
+          width: "100%",
+        }}
+      />
+      <TextInput
+        placeholder="Senha"
+        style={{
+          height: 54,
+          borderColor: "#000",
+          borderWidth: 1,
+          borderRadius: 7,
+          paddingHorizontal: 20,
+          width: "100%",
+        }}
+      />
+      <Button title="Entrar" />
     </View>
   );
 }
