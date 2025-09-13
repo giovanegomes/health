cacc5f1043b45afb95b3e1e65c3a527b08a2159f
Giovane Aires Gomes <giovaneag17@outlook.com>
2025-09-13 15:57:50 -0300

    feat: add ExerciseScreen and integrate into drawer navigation

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,11 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 import { HomeScreen } from "../screens/home";
+import { ExerciseScreen } from "../screens/exercises";
 
 export type Routes = {
   home: undefined;
+  exercises: undefined;
 };
 
 const Drawer = createDrawerNavigator<Routes>();
@@ -19,8 +21,13 @@ export function DrawerRouter() {
       >
         <Drawer.Screen
           name="home"
-          options={{ title: "Health App" }}
+          options={{ title: "Health App", drawerLabel: "Início" }}
           component={HomeScreen}
+        />
+        <Drawer.Screen
+          name="exercises"
+          options={{ title: "Exercícios" }}
+          component={ExerciseScreen}
         />
       </Drawer.Navigator>
     </NavigationContainer>
